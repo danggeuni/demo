e98ede5d52987a7c3b4a6cb35bd63725481f91b3
danggeuni <dydrnxkeh@naver.com>
2024-02-15 11:41:43 +0900

    encrypt - catch change : throw error

--- src/main/java/com/example/demo/util/EncryptionUtil.java
+++ src/main/java/com/example/demo/util/EncryptionUtil.java
@@ -24,7 +24,7 @@ public class EncryptionUtil {
             result = sb.toString();
 
         } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
 
         return result;
